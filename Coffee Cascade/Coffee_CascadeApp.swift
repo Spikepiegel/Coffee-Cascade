6f2aee52905b46cc0bd0a6d14dec1edb9799772b
Spikespiegel <Nikolkaaman@yandex.ru>
2024-07-07 02:39:15 +0300

    Initial Commit

---
+++ Coffee Cascade/Coffee_CascadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_CascadeApp.swift
+//  Coffee Cascade
+//
+//  Created by Николай Лермонтов on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_CascadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
